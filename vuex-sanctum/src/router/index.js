94314cccd55a43a3a84c54466b7f2340b3841f2e
Pryya Haseny <nnpryya@gmail.com>
2020-08-28 13:53:23 +0700

    Penambahan route

--- vuex-sanctum/src/router/index.js
+++ vuex-sanctum/src/router/index.js
@@ -2,10 +2,16 @@
 import Vue from 'vue'
 
 //import vue router
-import VueRouter from 'vue-router'
-
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import VueSweetalert2 from 'vue-sweetalert2';
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
 //use vue router on vue
 Vue.use(VueRouter)
+Vue.use(VueSweetalert2);
+Vue.use(VueAxios, axios);
 
 //create object instance vue router
 const router = new VueRouter({
@@ -26,7 +32,30 @@ const router = new VueRouter({
             name: 'dashboard',
             component: () =>
                 import ( /* webpackChunkName: "Auth" */ "@/views/dashboard/Index"),
-        }
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: () =>
+                import ( /* webpackChunkName: "Auth" */ "@/views/user/Index"),
+        },
+        {
+            path: '/user/create',
+            name: 'create',
+            component: () =>
+                import ( /* webpackChunkName: "Auth" */ "@/views/user/Create"),
+        },
+        {
+            path: '/user/show/:id',
+            name: 'show',
+            component: () =>
+                import ( /* webpackChunkName: "Auth" */ "@/views/user/Show"),
+        }, {
+            path: '/user/edit/:id',
+            name: 'edit',
+            component: () =>
+                import ( /* webpackChunkName: "Auth" */ "@/views/user/Edit"),
+        },
     ],
     mode: 'history'
 })
